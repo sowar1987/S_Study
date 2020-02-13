5fe8e4f68a5f402254078b99f113b63438a90221
sowar1987 <178468696@qq.com>
2020-02-14 00:37:09 +0800

    add new folder

---
+++ 02-Python/PythonProjects/opms/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   test.py
+@Time    :   2020/02/12 19:51:03
+@Author  :   sowar 
+@Version :   1.0
+@Contact :   178468696@qq.com
+'''
+# Start typing your code from here
+
+import sys
+print(sys.version)
+print("hello world!")
+for i in range(10):
+    print(i)
+
+
+def x():
+    pass
+
+
+def y():
+    pass
+a=123
